e2fa97945b31a51787a39c055a5604806a5f778b
MiguelEXE <miguellencina13@gmail.com>
2021-07-01 00:04:02 -0300

    add pathfinding.js (PATHFINDING NOT DONE YET)

--- diepioutils.user.js
+++ diepioutils.user.js
@@ -6,8 +6,6 @@
 // @match        https://diep.io/
 // @icon         https://diep.io/favicon.ico
 // @grant        unsafeWindow
-// @grant        GM_getValue
-// @grant        GM_setValue
 // @run-at       document-start
 // @homepage     https://github.com/MiguelEXE/DiepIOUtilities
 // @supportURL   https://github.com/MiguelEXE/DiepIOUtilities/issues
@@ -15,70 +13,83 @@
 // @updateURL    https://github.com/MiguelEXE/DiepIOUtilities/raw/master/diepioutils.user.js
 // ==/UserScript==
 
-const INPUT = {
-    leftMouse: 0b000000000001,
-    upKey: 0b000000000010,
-    leftKey: 0b000000000100,
-    downKey: 0b000000001000,
-    rightKey: 0b000000010000,
-    godMode: 0b000000100000,
-    suicide: 0b000001000000,
-    rightMouse: 0b000010000000,
-    instantUpg: 0b000100000000,
-    gamepad: 0b001000000000,
-    switchclass: 0b010000000000,
-    constantOfTrue: 0b100000000000,
-};
-const COLORS = [
-    'BASE_GRAY',
-    'BARREL_GRAY',
-    'BODY_BLUE',
-    'TEAM_BLUE',
-    'TEAM_RED',
-    'TEAM_PURPLE',
-    'TEAM_GREEN',
-    'SHINY_GREEN',
-    'SQUARE_YELLOW',
-    'TRIANGLE_RED',
-    'PENTAGON_PURPLE',
-    'CRASHER_PINK',
-    'CLOSER_YELLOW',
-    'SCOREBOARD_GREEN',
-    'MAZEWALL_GRAY',
-    'FFA_RED',
-    'NECRO_ORANGE',
-    'FALLEN_GRAY',
-    'GLITCH',
-];
-
-
 (function() {
     'use strict';
-    let websockets = [];
+    const INPUT = {
+        leftMouse: 0b000000000001,
+        upKey: 0b000000000010,
+        leftKey: 0b000000000100,
+        downKey: 0b000000001000,
+        rightKey: 0b000000010000,
+        godMode: 0b000000100000,
+        suicide: 0b000001000000,
+        rightMouse: 0b000010000000,
+        instantUpg: 0b000100000000,
+        gamepad: 0b001000000000,
+        switchclass: 0b010000000000,
+        constantOfTrue: 0b100000000000,
+    };
+    const MEMORY = {
+        u8: undefined,
+        u16: undefined,
+        u32: undefined,
+        i8: undefined,
+        i16: undefined,
+        i32: undefined,
+        f32: undefined       
+    };
+    
+    //supahero1's implementation, go checkout his websocket + memory hook implementation: https://github.com/supahero1/diep.io/tree/master/working-with-diep/dig.userscript.js
+    let postRun;
+    let trye = 0;
+    function hook(a, b, c) {
+        Object.defineProperty(a, b, {
+            set: c,
+            configurable: true,
+            enumerable: true
+        });
+    }
+    Object.defineProperty(Object.prototype, "postRun", {
+        get: function(){
+            return postRun;
+        },
+        set: function(to){
+            postRun = to;
+            if(Object.getOwnPropertyNames(this).length == 0 && trye++ == 1){
+                hook(this, "asm", function(to) {
+                    if(to == null) return;
+                    delete this.asm;
+                    this.asm = to;
+                    MEMORY.u8  = this.HEAPU8;
+                    MEMORY.u16 = this.HEAPU16;
+                    MEMORY.u32 = this.HEAPU32;
+                    MEMORY.i8  = this.HEAP8;
+                    MEMORY.i16 = this.HEAP16;
+                    MEMORY.i32 = this.HEAP32;
+                    MEMORY.f32 = this.HEAPF32;
+                });
+            }
+        },
+        configurable: true,
+        enumerable: true
+    });
+    //end memory hook
+    unsafeWindow.MEMORY = MEMORY;
+    unsafeWindow.websockets = [];
     unsafeWindow.WebSocket = new Proxy(unsafeWindow.WebSocket, {
         construct: function(target, url){
             let socket = new target(url);
-            let i = websockets.push(websockets)-1;
-            socket.addEventListener("close", () => delete websockets[i]);
+            let i = unsafeWindow.websockets.push(socket)-1;
+            socket.addEventListener("close", () => delete unsafeWindow.websockets[i]);
             return socket;
         }
     });
 
     unsafeWindow.document.title += " (utilities loaded)";
 
-    let settings = GM_getValue("setttings");
-    if(!settings) {
-        settings = {
-            showPlayersInMap: false,
-            simpleAimBot: false,
-            advancedAimBot: false
-        }
-        GM_setValue("settings", settings);
-    }
-
     let console = document.createElement("div");
     let logs = document.createElement("div");
-    let input = document.createElement("div");
+    let input = document.createElement("input");
 
     let notHided = "0.6%";
     let hided = "-28%";
@@ -97,10 +108,13 @@ const COLORS = [
     logs.style.height = "90%";
 
     input.style.backgroundColor = "#fff";
-    input.style.width = "100%";
-    input.style.height = "10%";
-    input.innerText = "Press / to type an command.";
+    input.style.width = "98.95%";
+    input.style.height = "9.3%";
+    input.style.border = "none";
+    input.placeholder = "Press / to type an command.";
 
+
+    input.disabled = true;
     let info = document.createElement("div");
     info.style.color = "#0000ff";
     info.innerText = "Welcome to Diep.io Utilities. Press enter to hide/show the console.";
@@ -117,13 +131,12 @@ const COLORS = [
         }
     }
 
-    function pathfind(x,y){
-        
-    }
-
     function commandHandler(command=""){
-        let cmd = command.split(" ").pop();
+        let cmd = command.split(" ")[0];
         let args = command.split(" ").slice(1);
+
+        unsafeWindow.console.log(cmd);
+        unsafeWindow.console.log(args);
 
         if(cmd == "help"){
             return `help -- shows all command list
@@ -147,7 +160,7 @@ const COLORS = [
     function commandRegister(command=""){
         command = command.toLowerCase();
         let commandDiv = document.createElement("div");
-        commandDiv.innerHTML = `<p style="color:#ff4500;">> ${command.split(" ").pop()}</p><p style="color:#00ff00;">${command.split(" ").slice(1).join(" ")}</p>`;
+        commandDiv.innerHTML = `<span style="color:#dd3000;">> ${command.split(" ")[0]}</span> <span style="color:#00bb00;">${command.split(" ").slice(1).join(" ")}</span>`;
 
         logs.appendChild(commandDiv);
         
@@ -159,40 +172,30 @@ const COLORS = [
 
     let isTyping = false;
     unsafeWindow.addEventListener("keydown", e => {
-        if(e.keyCode == 13){
-            unsafeWindow.console.log(isTyping);
-            if(!isTyping){
-                toggleConsole();
-            }
-        }
-    });
-    unsafeWindow.addEventListener("keydown", e => {
-        if(isHided) return;
-        if((e.keyCode == 191)&&!isTyping){
+        if(e.key == "/" && !isTyping){
             e.preventDefault();
             e.stopPropagation();
-            input.innerText = "";
+            input.placeholder = "";
             isTyping = true;
-        }else if((e.keyCode == 13)&&isTyping){
+        }else if(e.key == "Enter" && isTyping){
             e.preventDefault();
             e.stopPropagation();
-            commandRegister(input.innerText);
-            input.innerText = "Press / to type an command.";
+            commandRegister(input.value);
+            input.placeholder = "Press / to type an command.";
+            input.value = "";
             isTyping = false;
-        }else if((e.keyCode == 8)&&isTyping){
+        }else if(e.key == "Backspace"){
             e.preventDefault();
             e.stopPropagation();
-            let remove = input.innerText.split("");
-            remove[remove.length-1]="";
-            input.innerText=remove.join("");
-        }else if((e.keyCode == 32)&&isTyping){
+            input.value = input.value.split("").slice(0, input.value.split("").length-1).join("");
+        }else if(e.key.length > 1 && isTyping){
             e.preventDefault();
             e.stopPropagation();
-            input.innerText+=" ";
+            return;
         }else if(isTyping){
             e.preventDefault();
             e.stopPropagation();
-            input.innerText+=String.fromCharCode(e.keyCode).toLowerCase();
+            input.value+=e.key;
         }
     });
 
